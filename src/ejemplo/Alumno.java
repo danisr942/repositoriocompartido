b5e3ac9412fd9a786132a6e6bc1d0dc39c2e527a
user <user@1DAMPC3>
2019-02-26 09:39:28 +0100

    Merge origin/master
    He creado una nueva clase

---
+++ src/ejemplo/Alumno.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo;
+
+/**
+ *
+ * @author user
+ */
+class Alumno 
+{
+    private String nombre, apellido1, apellido2;
+    private int edad;
+    private double nota;
+
+    public Alumno(String nombre, String apellido1, String apellido2, int edad, double nota) {
+        this.nombre = nombre;
+        this.apellido1 = apellido1;
+        this.apellido2 = apellido2;
+        this.edad = edad;
+        this.nota = nota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+}
